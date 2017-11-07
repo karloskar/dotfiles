5d4f804190934cc5a06efb1eba20700fd28199ce
KO. Mattsson <karloskar@ponty.se>
2017-11-07 12:47:20 +0100

    Remove vundle from Vim install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -2,23 +2,20 @@
 
 echo "Installing..."
 
-# Do vundle install, should be handled from within vimrc
-vim +q
-
-ln -sf ~/.vim/bundle/xmledit/ftplugin/xml.vim ~/.vim/bundle/xmledit/ftplugin/html.vim
+ln -sf ~/.vim/plugged/xmledit/ftplugin/xml.vim ~/.vim/plugged/xmledit/ftplugin/html.vim
 # Make xmledit autocomplete jinja files
-if [ -f ~/.vim/bundle/xmledit/ftplugin/jinja.vim ]
+if [ -f ~/.vim/plugged/xmledit/ftplugin/jinja.vim ]
 then
   echo "jinja.vim symlink already exists"
 else
-  ln -s ~/.vim/bundle/xmledit/ftplugin/html.vim ~/.vim/bundle/xmledit/ftplugin/jinja.vim
+  ln -s ~/.vim/plugged/xmledit/ftplugin/html.vim ~/.vim/plugged/xmledit/ftplugin/jinja.vim
 fi
 # Make xmledit autocomplete javascript files (for JSX)
-if [ -f ~/.vim/bundle/xmledit/ftplugin/javascript.jsx.vim ]
+if [ -f ~/.vim/plugged/xmledit/ftplugin/javascript.jsx.vim ]
 then
   echo "javascript.jsx.vim symlink already exists"
 else
-  ln -s ~/.vim/bundle/xmledit/ftplugin/html.vim ~/.vim/bundle/xmledit/ftplugin/javascript.vim
+  ln -s ~/.vim/plugged/xmledit/ftplugin/html.vim ~/.vim/plugged/xmledit/ftplugin/javascript.vim
 fi
 
 echo "Done."
